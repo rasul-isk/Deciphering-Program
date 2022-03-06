84fe17560dbf38c47f629448832e729aa34f5758
Rasul Iskandarov <73297277+rasul-isk@users.noreply.github.com>
2022-03-06 14:30:03 +0000

    some error fixes in source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,8 @@
+//Name:		Rasul Iskandarov
+//Date:		28/02/2022
+
 #include <iostream>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <time.h> 
 #include <string>
 #include <fstream>
@@ -184,6 +187,7 @@ void Part_2(string cyphered)
 	string encypheredText, answer = "n";
 	int shift = 0;
 	int trueWordPercent = 0;
+	int accuracy = 0;
 
 	cout << "---------------------------------------------------------------" << endl;
 	cout << "Part 2 - Code Breaker:" << endl << endl;
@@ -192,34 +196,55 @@ void Part_2(string cyphered)
 
 	cout << "Cyphered text looks like: " << endl << endl << cyphered << endl << endl;
 
-	while (answer == "n")
+	cout << "How much decyphering accuracy do you want? (0-100)" << endl;
+	cout << "More than or equal to ";
+	cin >> accuracy;
+	cout << endl << endl;
+
+	while (true)
 	{
-		encypheredText = "";
-		encypheredText = shiftText(cyphered, shift);
-		trueWordPercent = countWords(encypheredText);
-
-		if (trueWordPercent > 90)
+		if (accuracy <= 100 && accuracy >= 0)
 		{
-			cout << "Normal text looks like: " << endl << endl << encypheredText << endl << endl;
-			cout << "Left shift equals to " << abs(shift) << " | Right Words Percentage: " << trueWordPercent << endl << endl;
-			cout << "Is encyphered text displayed correctly? Y/n?" << endl;
-			cin >> answer;
+			while (answer == "n" && shift !=-100)
+			{
+				encypheredText = "";
+				encypheredText = shiftText(cyphered, shift);
+				trueWordPercent = countWords(encypheredText);
 
-			cout << endl << endl;
+				if (trueWordPercent >= accuracy && shift != -100)
+				{
+					cout << "Normal text looks like: " << endl << endl << encypheredText << endl << endl;
+					cout << "Left shift equals to " << abs(shift) << " | Right Words Percentage: " << trueWordPercent << endl << endl;
+					cout << "Is encyphered text displayed correctly? Y/n?" << endl;
+					cin >> answer;	
+
+					cout << endl << endl;
+				}
+
+				shift--;
+			}
+			if (shift == -100)
+				cout << "Accuracy not found." << endl << endl;
+			break;
 		}
-
-		shift--;
+		else
+		{
+			cout << "Please enter correct number. From 0 to 100." << endl << endl;
+		}
 	}
 }
 
 void Part_3(string originText, string cypherText)
 {
-	int maxFreq[2] = { 0, 0 };
+	int maxFreq[2];
+	int request = 0;
+	int repeat = 0;
 	string frequence[2] = { "", ""};
 	string result = "";
 	int shift = 0;
 	map<string, int> Freq;
 	map<string, int> FreqCypher;
+	string answer = "n";
 
 	cout << "---------------------------------------------------------------" << endl;
 	cout << "Part 3 - Better guess:" << endl << endl;
@@ -227,38 +252,68 @@ void Part_3(string originText, string cypherText)
 	fillMap(Freq, originText);
 	fillMap(FreqCypher, cypherText);
 
-	for (auto& it : Freq) {
-		if (maxFreq[0] < it.second)
+	while (answer == "n" && repeat < 20)
+	{
+		repeat++;
+		request = 0;
+		
+		cout << "Enter maximum frequency can be used (minimum input 5): ";
+		cin >> request;
+		cin.ignore();
+		cout << endl << endl;
+		if (request > 5)
 		{
-			maxFreq[0] = it.second;
-			frequence[0] = it.first;
+			maxFreq[0] = 0;
+			maxFreq[1] = 0;
+
+			for (auto& it : Freq) {
+				if (maxFreq[0] < it.second && it.second <= request)
+				{
+					maxFreq[0] = it.second;
+					frequence[0] = it.first;
+				}
+			}
+
+			for (auto& it : FreqCypher) {
+				if (maxFreq[1] < it.second && it.second <= request)
+				{
+					maxFreq[1] = it.second;
+					frequence[1] = it.first;
+				}
+			}
+
+			if (maxFreq[0] == maxFreq[1])
+			{
+				shift = decypher(frequence[0], frequence[1]);
+
+				cout << "Letter/word with high and same frequency was found!" << endl << endl;
+				cout << "This letter / word: " << endl;
+				cout << frequence[0] << " and " << frequence[1] << endl << endl;
+				cout << "Both has frequency of ";
+				cout << maxFreq[0] << endl << endl;
+				cout << "Your shift to decypher text is ";
+				cout << shift << endl << endl << "Decyphered Text:" << endl << shiftText(cypherText, shift) << endl;
+
+				cout << "Is encyphered text displayed correctly? Y/n?" << endl;
+				cin >> answer;
+			}
+			else
+			{
+				cout << "Letter/word with high and same frequency was not found. Try again.\n\n";
+				answer = "n";
+			}
+		}
+		else
+		{
+			cout << "Please enter correct number. From 0 to infinity." << endl << endl;
+			answer = "n";
 		}
 	}
 
-	for (auto& it : FreqCypher) {
-		if (maxFreq[1] < it.second)
-		{
-			maxFreq[1] = it.second;
-			frequence[1] = it.first;
-		}
-	}
+	if (repeat == 100)
+		cout << "Fail to decipher." << endl << endl;
 
-	if (maxFreq[0] == maxFreq[1])
-	{
-		shift = decypher(frequence[0], frequence[1]);
-
-		cout << "Letter/word with high and same frequency was found." << endl << endl;
-		cout << "This letter / word: " << endl;
-		cout << frequence[0] << endl << frequence[1] << endl << endl;
-		cout << "Both has frequency of ";
-		cout << maxFreq[0] << endl << endl;
-		cout << "Your shift to decypher text is ";
-		cout << shift << endl << endl << "Decyphered Text:" << endl << shiftText(cypherText, shift) << endl;
-	}
-	else
-	{
-		cout << "Letter/word with high and same frequency was not found. \n\n";
-	}
+	cout << endl << "Bye!" << endl << endl;
 }
 
 void main()
